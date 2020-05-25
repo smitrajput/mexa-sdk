2138bfc970fc0720e7f0ceb1a29395ba3efcc5d5
smitrajput <smitrajputtd@gmail.com>
2020-05-25 15:31:29 +0530

    Add independent reverts

--- gitcoin/bundlez/contracts/account/BaseAccount.sol
+++ gitcoin/bundlez/contracts/account/BaseAccount.sol
@@ -105,10 +105,10 @@ contract BaseAccount is AccountState, AccountInitialize, TokenReceiverHooks {
         (bool success, bytes memory res) = _destination.call.gas(_gasLimit).value(_value)(_data);
 
         // Get the revert message of the call and revert with it if the call failed
-        if (!success) {
-            string memory _revertMsg = _getRevertMsg(res);
-            revert(_revertMsg);
-        }
+        // if (!success) {
+        //     string memory _revertMsg = _getRevertMsg(res);
+        //     revert(_revertMsg);
+        // }
 
         return res;
     }
